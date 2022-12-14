84c43257fb017a5874c26bda47f67d0a53077907
Test1calCutter <108223454+Test1calCutter@users.noreply.github.com>
2022-12-14 21:21:13 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,17 +6,5 @@ $(document).ready(function(){
   $(".overlay p").toggleClass("open");
 	});
   
-  // Scroll Reveal
-  // ScrollReveal().reveal('.services .grid .card');
-  
-  $('.square.blue').on('inview', function(event, isInView) {
-    if (isInView) {
-      // element is now visible in the viewport
-      $(this).addClass("in-view");
-    } else {
-      // element has gone out of viewport
-      $(this).removeClass("in-view");
-    }
-  });
-});
+
  
